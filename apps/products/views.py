26ee73a611d56f5e6481ca20863c697d1e457d78
SRAKIB17 <rakibulssc5@gmail.com>
2023-04-21 22:52:35 +0600

    'done'

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('header.html')
+    return HttpResponse(template.render())
+
+# def index(request):
+#     return HttpResponse("Hello world!")
